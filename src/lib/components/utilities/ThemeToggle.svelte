7d034b0f774b9149d1e9986fac1caa3e192f745a
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-10-23 19:17:44 -0400

    Formatted Theme Toggler

--- src/lib/components/utilities/ThemeToggle.svelte
+++ src/lib/components/utilities/ThemeToggle.svelte
@@ -1,39 +1,44 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
-    let currentTheme = 'light';
-    $: iconThemeColor = currentTheme === 'light' ? '#000000' : '#ffffff'
+    import { onMount } from 'svelte';
 
     function toggleCurrentTheme() {
         // If on the server
-        if (typeof window === 'undefined')
-            return;
+        if (typeof window === 'undefined') return;
 
-        localStorage.theme = localStorage.theme === 'light' ? 'dark' : 'light'
-        updateBrowserTheme()
+        localStorage.theme = localStorage.theme === 'light' ? 'dark' : 'light';
+        updateBrowserTheme();
     }
 
     function updateBrowserTheme() {
         // If on the server
-        if (typeof window === 'undefined')
-            return;
+        if (typeof window === 'undefined') return;
 
-        let isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        let isDark =
+            localStorage.theme === 'dark' ||
+            (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
         if (isDark) {
-            document.documentElement.classList.add('dark')
+            document.documentElement.classList.add('dark');
         } else {
-            document.documentElement.classList.remove('dark')
+            document.documentElement.classList.remove('dark');
         }
-
-        currentTheme = isDark ? 'dark' : 'light'
     }
 
     onMount(updateBrowserTheme);
-
 </script>
 
 <button on:click={toggleCurrentTheme} type="button">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 stroke-neutral-900 dark:stroke-neutral-200">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-    </svg>      
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 stroke-neutral-900 dark:stroke-neutral-200"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+        />
+    </svg>
 </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,7 @@
 <script>
-    import { onMount } from 'svelte';
     import 'iconify-icon';
     import '../app.css';
     import ThemeToggle from '$lib/components/utilities/ThemeToggle.svelte';
-
-    onMount(updateBrowserTheme);
-
-    function toggleCurrentTheme() {
-        // If on the server
-        if (typeof window === 'undefined')
-            return;
-
-        localStorage.theme = localStorage.theme === 'light' ? 'dark' : 'light'
-        updateBrowserTheme()
-    }
-
-    function updateBrowserTheme() {
-        // If on the server
-        if (typeof window === 'undefined')
-            return;
-
-        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
-    }
-
 </script>
 
 <div class="w-screen h-screen bg-white dark:bg-stone-900 transition duration-150">
